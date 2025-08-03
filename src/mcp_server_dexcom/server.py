c554af9b920bc6eb322bb99a5e8cee669b1b4ff8
Max Weinzierl <supermaxman1@gmail.com>
2025-08-03 13:47:48 -0500

    Initial commit

--- src/mcp_server_dexcom/server.py
+++ src/mcp_server_dexcom/server.py
@@ -21,29 +21,30 @@ class GlucoseReading(BaseModel):
 
 
 @mcp.tool(
-    description="Fetches the most recent glucose reading from the Dexcom Share website."
+    description="Fetches the most recent glucose reading from the Dexcom Share website. Time is in local time and units are in mg/dL."
 )
-def get_latest_glucose_reading() -> GlucoseReading:
+def get_latest_glucose_reading() -> str:
     """Fetches the most recent glucose reading from the Dexcom Share website."""
     latest_reading = dexcom.get_latest_glucose_reading()
     if latest_reading is None:
         raise Exception("No glucose reading found")
-    return GlucoseReading(
+    item = GlucoseReading(
         reported_at=latest_reading.datetime,
         value=latest_reading.value,
         unit="mg/dL",
         trend_arrow=latest_reading.trend_arrow,
         trend_description=latest_reading.trend_description,
     )
+    return f"{item.reported_at.strftime('%Y-%m-%d %H:%M')} - {item.value} {item.unit} ({item.trend_arrow} {item.trend_description})"
 
 
 @mcp.tool(
-    description="Fetches prior 24 hours of glucose readings from the Dexcom Share website."
+    description="Fetches prior 24 hours of glucose readings from the Dexcom Share website. Time is in local time and units are in mg/dL."
 )
-def get_glucose_readings() -> list[GlucoseReading]:
+def get_glucose_readings() -> str:
     """Fetches prior 24 hours of glucose readings from the Dexcom Share website."""
     readings = dexcom.get_glucose_readings()
-    return [
+    items = [
         GlucoseReading(
             reported_at=reading.datetime,
             value=reading.value,
@@ -54,6 +55,12 @@ def get_glucose_readings() -> list[GlucoseReading]:
         # reverse the list to get the most recent reading last
         for reading in readings[::-1]
     ]
+    resp_lines = [
+        f"{reading.reported_at.strftime('%Y-%m-%d %H:%M')},{reading.value}"
+        for reading in items
+    ]
+    resp_lines.insert(0, "reported_at (local time),value (mg/dL)")
+    return "\n".join(resp_lines)
 
 
 def main() -> None:
